3238c7e75f092a0e89874cf1a9304d1c29605378
gaganadeepthi25-svg <gaganadeepthi25@gmail.com>
2025-09-06 15:54:25 +0530

    functions

---
+++ function.js
@@ -0,0 +1,25 @@
+function greet(){
+    console.log("welcome to mern stack")
+}
+greet()
+
+function sum(x,y){
+    return x+y
+}
+let result=sum(2,3)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+const multiply=function(x,y){
+    return x*y
+}
+console.log(multiply(5,3))
+//arrow function
+const square =(x) =>x*x
+    console.log(square(5))
+const subtract=(x,y)=>{
+    console.log(x,y)
+    return x-y
+}
+console.log(subtract(3,2))
