7a4da357e88aa3590452a2cf530913d1d5fb210c
gaganadeepthi25-svg <gaganadeepthi25@gmail.com>
2025-09-06 12:33:48 +0530

    learned basics of js

--- practice.js
+++ practice.js
@@ -1,1 +1,35 @@
 console.log("hello world, deepthi")
+
+let name="deepthi";
+console.log(name)
+
+let num=57
+console.log(num)
+
+let i=false
+console.log(i)
+
+let x
+console.log(x)
+
+let y=null
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof i)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"deepthi",
+    age:18
+
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
